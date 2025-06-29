2007831b0af4b763e8e8322f45407bc27286a50b
DuVC4310 <vfranciscoeduardo055@gmail.com>
2025-06-29 11:07:37 -0300

    Create notasAluno.py

---
+++ notasAluno.py
@@ -0,0 +1,10 @@
+#soma de notas e média final
+
+media= 7
+nota1= float(input("informe a sua primeira nota:"))
+nota2= float(input("informe a sua segunda nota:"))
+nota3= float(input("informe a sua terceira nota:"))
+
+soma= nota1+nota2+nota3
+media= soma/3
+print(f"a soma das notas é:{soma} e sua média ficou={media}")
